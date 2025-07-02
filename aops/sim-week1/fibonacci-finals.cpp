6577705ef3129b38d2b49c36db803e3dfd3bb953
Melissa Yin <melissayin8@gmail.com>
2025-07-02 12:02:36 -0500

    solved fibonacci finals, start mooximum occowpancy

--- aops/sim-week1/fibonacci-finals.cpp
+++ aops/sim-week1/fibonacci-finals.cpp
@@ -1,40 +1,70 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+/*
+x
+y
+1x + 1y
+1x + 2y
+2x + 3y
+3x + 5y
+5x + 8y
+*/
+int nums[2];
+int a, b, c, d;
+
+void fastDouble(long long n, int r[]) {
+    if (n == 0) {
+        r[0] = 0;
+        r[1] = 1;
+        return;
+    }
+
+    fastDouble(n / 2, r);
+
+    a = r[0];
+    b = r[1];
+    c = 2 * b - a;
+
+    if (c < 0) {
+        c += 1000;
+    }
+
+    c = (a * c) % 1000;
+
+    d = (a * a + b * b) % 1000;
+
+    if (n % 2 == 0) {
+        r[0] = c;
+        r[1] = d;
+    }
+    else {
+        r[0] = d;
+        r[1] = c + d;
+    }
+    
+}
 int main () {
     ios_base::sync_with_stdio(false);
     cin.tie(0);
 
     long long J0, J1, N, K;
     cin >> J0 >> J1 >> N >> K;
-
-    /*
-    x 0 
-    0x + y 1 0
-    1x + y 2 
-    1x + 2y 3 
-    2x + 3y 4
-    3x + 5y 5
-    5x + 8y
-    8x + 13y
     
-    */
-    long long prev = J1;
-    long long prev2 = J0;
-    long long curr = 0;
-
-    long long fib = (pow((1 + sqrt(5.0)) / 2.0 , N) + pow((1 - sqrt(5.0)) / 2.0, N)) / sqrt(5.0);
-    fib = fib % 1000;
-    
+    int ans = 0;
+    fastDouble(N - 1, nums);
+    //cout << nums[0] << ' ' << nums[1] << "\n";
+    ans += nums[0] * J0;
+    ans += nums[1] * J1;
 
     if (K == 1) {
-        cout << curr % 10 << "\n";
+        cout << ans % 10 << "\n";
     }
     else if (K == 2) {
-        cout << curr % 100 << "\n";
+        cout << ans % 100 << "\n";
     }
     else {
-        cout << curr % 1000 << "\n";
+        cout << ans % 1000 << "\n";
     }
     /*
     for (long long i = 0; i < N - 1; i++) {
